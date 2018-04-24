21cd9b1b1fa3e6a6d4f672decad44717a89a63c9
Jeff Clarke <jeff@clarkey.ca>
2018-04-24 12:20:21 +0100

    Added screensaver option

--- MMM-ProfileCarousel.js
+++ MMM-ProfileCarousel.js
@@ -5,11 +5,21 @@ Module.register("MMM-ProfileCarousel", {
     carouselDelay: 10000, //10 seconds
     carouselResumeDelay: 1 * 60 * 1000, //1 minute
     keyInputIgnoreDelay: 1500,
+    useScreensaver: true,
+    screensaverProfile: "L_WORDCLOCK",
+    screensaverTimeout: 10.25 * 60 * 1000 //10m 15s
   },
 
   currentProfile: 0,
   carouselTimer: null,
   carouselResumeDelayTimer: null,
+  screensaverTimer: null,
+  screensaverActive: false,
+  screensaverBlackoutPeriodActive: false,
+
+  getScripts: function() {
+    return ["moment.js"];
+  },
 
   start: function() {
 
@@ -20,21 +30,51 @@ Module.register("MMM-ProfileCarousel", {
       if (!self.isTransitioning) {
         switch (key.code) {
           case "ArrowRight":
-            self.stopCarousel();
-            self.nextProfile();
-            self.resetResumeCarouselTimer();
+            self.navRight();
             break;
           case "ArrowLeft":
-            self.stopCarousel();
-            self.previousProfile();
-            self.resetResumeCarouselTimer();
+            self.navLeft();
             break;
+          case "Space":
+            self.navSelect();
+            break;          
         }
       }
     });
 
     this.startCarousel();
 
+    if (this.config.useScreensaver) {
+      this.startScreensaverTimer();
+
+      if (this.config.screensaverBlackoutPeriod) {      
+        this.startScreensaverBlackoutHeartbeat();
+      }
+
+    }
+
+  },
+
+  //Navigation input functions
+  navRight: function() {
+    this.resetScreensaver();
+    this.stopCarousel();
+    this.nextProfile();
+    this.resetResumeCarouselTimer();
+  },
+
+  navLeft: function() {
+    this.resetScreensaver();
+    this.stopCarousel();
+    this.previousProfile();
+    this.resetResumeCarouselTimer();
+  },
+
+  navSelect: function() {
+    if (this.screensaverActive) {
+      this.resetScreensaver();
+      this.resetResumeCarouselTimer();
+    }
   },
 
   nextProfile: function() {
@@ -90,6 +130,63 @@ Module.register("MMM-ProfileCarousel", {
       self.nextProfile();
       self.startCarousel();
     }, this.config.carouselResumeDelay);
+  },
+
+  startScreensaverTimer: function() {
+    var self = this;
+    self.screensaverTimer = window.setTimeout(function() {
+      self.enableScreensaver();
+    }, this.config.screensaverTimeout);
+  },
+
+  enableScreensaver: function() {
+    window.clearTimeout(this.carouselResumeDelayTimer);
+    this.stopCarousel();
+    this.sendNotification("CURRENT_PROFILE", this.config.screensaverProfile);
+    this.screensaverActive = true;
+  },
+
+  stopScreensaver: function() {
+    window.clearTimeout(this.screensaverTimer);
+    if (this.screensaverActive) {
+      this.screensaverActive = false;
+      this.setProfile();
+    }
+  },
+
+  resetScreensaver: function() {
+    this.stopScreensaver();
+    if (this.config.useScreensaver) {
+      this.startScreensaverTimer();
+    }
+  },
+
+  startScreensaverBlackoutHeartbeat: function() {
+    var self = this;
+    window.setInterval(function() {
+      self.screensaverBlackoutHeartbeatTick();
+    }, 10000);
+  },
+
+  screensaverBlackoutHeartbeatTick: function() {
+    var hourNow = moment().hour();
+    if (this.screensaverBlackoutPeriodActive) {
+      //check to see if the time is outside the range
+      if (hourNow < this.config.screensaverBlackoutPeriod.start || hourNow >= this.config.screensaverBlackoutPeriod.end) {
+
+        this.startScreensaverTimer();
+        this.screensaverBlackoutPeriodActive = false;
+      }
+    } else {
+      //check to see if the time is within the blackout range
+      if (hourNow >= this.config.screensaverBlackoutPeriod.start && hourNow < this.config.screensaverBlackoutPeriod.end) {
+
+        this.stopScreensaver();
+        this.screensaverBlackoutPeriodActive = true;
+
+      }
+
+    }   
   }
 
 
